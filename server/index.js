fad555a0aafa47bc8235b2d42ea97d747eface97
siddharth <110058016+sidish15@users.noreply.github.com>
2024-03-22 17:46:42 +0530

    add pre installation

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.attendance.js"
 import cookieParser from "cookie-parser";
-
+import path from "path";
 dotenv.config();
 mongoose.connect(process.env.MONGO)
         .then(() => {
@@ -16,12 +16,18 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
-app.listen(4000, () => {
-        console.log(`Server is running on PORT 4000`);
-})
+
+
 
 app.use('/api/auth', authRouter)
 app.use("/api/user", userRouter)
+
+app.use(express.static(path.join(__dirname,'client/dist')))
+
+app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 app.use((err, req, res, next) => {
         const statusCode = err.statusCode || 500;
         const message = err.message || 'Internal Server Error';
@@ -30,4 +36,12 @@ app.use((err, req, res, next) => {
                 statusCode,
                 message
         })
+})
+
+
+const __dirname=path.resolve()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+        console.log(`Server is running on PORT 4000`);
 })
